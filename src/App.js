19a20ab9cfd908d950fc093f9a137fba8909b59e
Jemimah <jemynagasha@gmail.com>
2022-09-07 21:43:44 +0300

    sign up modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import PrimaryButton, {SecondaryButton, TertiaryButton} from './components/Button';
-import { GlobalStyle } from './utils';
+import React, { useState } from 'react';
+// import { PrimaryButton, SecondaryButton, TertiaryButton} from './components/Button';
+import { GlobalStyle, darkTheme, defaultTheme } from './utils';
+import { ThemeProvider } from 'styled-components';
+import { SignUpModal } from "./components/Modals";
 
 function App() {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
   return (
-    <div>
-      <PrimaryButton modifiers={["small", "success", "primaryButtonSuccess"]}>Hello World!</PrimaryButton>
-      <SecondaryButton modifiers={["large", "warning", "secondaryButtonWarning"]}>Goodbye World!</SecondaryButton>
-      <TertiaryButton modifiers={["error", "tertiaryButtonError"]}>Hey World!</TertiaryButton>
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px"
+        }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{
+          margin: "0 16px 24px",
+          padding: "8px",
+          background: "none",
+          cursor: "pointer",
+          border: "2px solid #000",
+          marginTop: "60px"
+        }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <div 
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <SignUpModal />
+      </div>
       <GlobalStyle />
-    </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import SignIn from "./illustrations/SignIn.svg";
+import SignUp from "./illustrations/SignUp.svg";
+
+export * from "./Icons/CloseIcon";
+export const Illustrations = {
+  SignUp,
+  SignIn
+};
